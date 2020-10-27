c13c7776b7b14247eb28b028cd86e2913b596d97
Nope <nope@pop-os.localdomain>
2020-10-27 17:32:27 -0400

    end

--- MazeSolver.hpp
+++ MazeSolver.hpp
@@ -6,14 +6,12 @@
 class MazeSolver : public Window {
 private:
 	Maze* maze;
-	// TODO : Declarations.
 	Stack<Position*> myStack;
 
 public:
 	MazeSolver(Maze* maze) {
 		this->maze = maze;
 		srand(time(NULL));
-		// TODO : Instanciations.
 		for (int x = 0; x < 53; x++)
 			for (int y = 0; y < 53; y++)
 				if (maze->getSquare(x, y) == Square::ENTRY) {
@@ -23,39 +21,28 @@ public:
 	}
 
 	~MazeSolver() {
-		// TODO : Liberations.
+		while (myStack.size()) {
+			delete myStack.top();
+			myStack.pop();
+		}
 		delete maze;
 	}
 
 	void onUpdate() {
 		int dir = -1;
-		// TODO : Avancer d'un pas.
-		switch (myStack.top()->getDirectionCount()) {
-			case 0:
-				// TODO : pop until multiple choice
-				if (maze->getSquare(myStack.top()->x, myStack.top()->y) != Square::EXIT)
-					myStack.pop();
-			break;
-			case 1:
-				// TODO : fucking go
-				for (int i = 0; i < 4; i++)
-					if (myStack.top()->directions[i] == true)
-						dir = i;
-			break;
-			case 2:
-			case 3:
-			case 4:
-				// TODO : choose a random path and fucking go
-				// 				Affecter dir a random
-				while (!(myStack.top()->directions[dir]))
-					dir = rand() % 4;
-			break;
+		if (!myStack.top()->getDirectionCount()) {
+			if (maze->getSquare(myStack.top()->x, myStack.top()->y) != Square::EXIT)
+				myStack.pop();
 		}
+		else if(myStack.top()->getDirectionCount() == 1) {
+			for (int i = 0; i < 4; i++)
+				if (myStack.top()->directions[i])
+					dir = i;
+		}
+		else
+			while (!(myStack.top()->directions[(dir = rand() % 4)]));
+
 		switch (dir) {
-			// mettre direction que je vais emprunter a faux
-			// se deplacer (nouvelle instance et empiler)
-			// determiner les directions possible
-			// mettre a faux d'ou je viens
 			case Position::NORTH:
 				myStack.top()->directions[Position::NORTH] = false;
 				myStack.push(new Position(myStack.top()->x, myStack.top()->y - 1));
@@ -84,16 +71,13 @@ public:
 	}
 
 	void onRefresh() {
-		// TODO : Afficher le labyrinthe.
 		for (int x = 0; x < 53; x++)
 			for (int y = 0; y < 53; y++)
 				drawSquare(maze->getSquare(x, y), x, y);
-		// TODO : Afficher le chemin parcouru.
 		drawSquare(Square::PATH, myStack.top()->x, myStack.top()->y);
 	}
 
 	void setDirections() {
-		// TODO : Update possible directions
 		myStack.top()->directions[Position::EAST] = maze->getSquare(myStack.top()->x + 1, myStack.top()->y) != Square::WALL;
 		myStack.top()->directions[Position::WEST] = maze->getSquare(myStack.top()->x - 1, myStack.top()->y) != Square::WALL;
 		myStack.top()->directions[Position::SOUTH] = maze->getSquare(myStack.top()->x, myStack.top()->y + 1) != Square::WALL;
